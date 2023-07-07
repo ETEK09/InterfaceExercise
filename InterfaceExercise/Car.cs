b5c75d899be9c355447391fe92740d0f3fb27319
Eric Caro <c.aroericm@gmail.com>
2023-07-07 13:01:43 -0500

    Exercise 1

---
+++ InterfaceExercise/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise
+{
+    public class Car : IVehicle , ICompany
+    {
+        public Car() 
+        { 
+        }
+
+        public double EngineSize { get; set; } = 4.6;
+        public string Make { get; set; } = "Dodge";
+        public string Model { get; set; } = "Charger";
+        public int Seatcount { get; set; } = 4;
+        public string CompanyName { get; set; } = "Chevy";
+        public string Motto { get; set; } = "Built Chevy Heavy";
+
+        public bool HasChangedGears { get; set; }
+
+
+
+        public void Drive()
+    {
+
+          Console.WriteLine($"{GetType().Name} now driving forward...");
+
+    }
+
+    public void Reverse()
+    {
+            if (HasChangedGears == true) 
+            {
+                Console.WriteLine($"{GetType().Name} now reversing . . .");
+                HasChangedGears = false;
+
+
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse until we change gears");
+            }
+
+        Console.WriteLine($"{GetType().Name} now driving forward...");
+
+    }
+
+    public void Park()
+    {
+        Console.WriteLine();
+
+    }
+
+    public void ChangeGears(bool isChanged)
+    {
+        HasChangedGears = isChanged;
+
+    }
+
+
+}
+}
---
+++ InterfaceExercise/Truck.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise
+{
+    internal class Truck : IVehicle , ICompany
+    {
+        public Truck() 
+        { 
+        }
+
+
+        public double EngineSize { get; set; } = 6;
+        public string Make { get; set; } = "Ford";
+        public string Model { get; set; } = "F150";
+        public int Seatcount { get; set; } = 4;
+        public string CompanyName { get; set; } = "Ford";
+        public string Motto { get; set; } = "Built Chevy Heavy";
+
+        public bool HasChangedGears { get; set; }
+
+        public bool HasFourWheelDrive { get; set; }
+
+
+
+        public void Drive()
+        {
+            if (HasFourWheelDrive == true) 
+                        {
+                Console.WriteLine($" wheel drive {GetType().Name} now driving forward...");
+            }
+            else 
+            {
+                Console.WriteLine($"{GetType().Name} now driving forward...");
+            }
+            
+
+        }
+
+        public void Reverse()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing . . .");
+                HasChangedGears = false;
+
+
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse until we change gears");
+            }
+
+            Console.WriteLine($"{GetType().Name} now driving forward...");
+
+        }
+
+        public void Park()
+        {
+            Console.WriteLine();
+
+        }
+
+        public void ChangeGears(bool isChanged)
+        {
+            HasChangedGears = isChanged;
+
+        }
+
+    }
+}
